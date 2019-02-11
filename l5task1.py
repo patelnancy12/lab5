6ad80fbfc299cd44aae4bcbe76a50faade1d42e1
patelnancy12 <Nancy.Patel@dal.ca>
2019-02-11 00:06:35 +0000

    Task done

--- l5task1.py
+++ l5task1.py
@@ -1,10 +1,24 @@
+import math
+
 class Point():
-  x = 0
-  y = 0
 
-  distance = 0
+  def __init__(self):
+    self.x = 10
+    self.y = 12
 
-  def distance_between_points(x,y):
+  def distance_between_points(self):
 
-    distance = y - x 
+    distance = math.sqrt(self.x**2 + self.y**2)
+    return distance
 
+  def print_point(self):
+    print ('(%g, %g)' % (self.x, self.y))
+
+
+
+p1 = Point()
+
+c = p1.distance_between_points()
+
+print(p1.distance_between_points())
+p1.print_point()
